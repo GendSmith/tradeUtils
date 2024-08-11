0e1e839faac5d21e845d3c3c27905b401b5d88a2
smithdeng <smithdeng@tencent.com>
2024-08-11 14:08:09 +0800

    feat:添加1分钟的k线数据采集

--- orderBook/kLine.js
+++ orderBook/kLine.js
@@ -1,1 +1,9 @@
-"kline@BTCUSDT@Binance@1m"
+import  {subscribeCoinAnkWS}  from "./request.js";
+
+const orderType ="kline@BTCUSDT@Binance@1m";
+
+subscribeCoinAnkWS(orderType, (data) => {
+    console.log(JSON.stringify(data));
+ });
+
+
